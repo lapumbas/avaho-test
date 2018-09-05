140bf099a9a412dcd270a8044d5a8f853bde017e
lapumbas <lapumbas@ya.ru>
2018-09-05 20:15:55 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import _ from 'lodash';
+
+import Button from './Button';
+import './style.css';
+
+class App extends React.Component {
+  state = {buttons: ['1', '2', '3']}
+
+  handleClick = evt => {
+    const buttons = this.state.buttons;
+    let first, rest;
+    [first, ...rest] = buttons;
+    rest.push(first)
+    this.setState({buttons: rest})
+  }
+
+  renderButtons = () => {
+    const buttons = this.state.buttons;
+    return buttons.map(item => <Button key={_.uniqueId()} click={this.handleClick} text={item} />)
+  }
+
+  render() {
+    return (
+      <div className="App">{this.renderButtons()}</div>
+    )
+  }
+}
+
+export default App;
